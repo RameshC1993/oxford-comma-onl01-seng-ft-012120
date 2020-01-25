f3e309a10b65852c689c8a74c46136a993f33b9e
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-25 19:22:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-  
+  my_str = ""
+ array.each do |item| 
+    if item == array.first
+        my_str += item
+    elsif item == array.last 
+        my_str += " and " + item
+    else
+        my_str += ", " + item
+    end
+ end
+ return my_str
 end
